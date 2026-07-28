6ac51bc749938ecdbad1b1e66a2343ac68b2a3fc
Jahnvi Srivastava <jahnvisrivastava0104@gmail.com>
2026-07-28 22:31:40 +0530

    Fix Render deployment

--- app.js
+++ app.js
@@ -23,6 +23,8 @@ app.use('/user',userRouter)
 app.use('/uploads',express.static('uploads'));
 app.use('/file', fileRouter);
 
-app.listen(3000,()=>{
-    console.log('server is running on port 3000');
-})
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
