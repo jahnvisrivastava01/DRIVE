eb28eb7e5f82ebe0698bfad0f6eff0407b15d368
Jahnvi Srivastava <jahnvisrivastava0104@gmail.com>
2026-07-30 12:35:57 +0530

    Update Firebase service account configuration

--- config/firebaseAdmin.js
+++ config/firebaseAdmin.js
@@ -1,7 +1,7 @@
 const { initializeApp, cert } = require("firebase-admin/app");
 const { getAuth } = require("firebase-admin/auth");
 
-const serviceAccount = require("./drive-95bb0-firebase-adminsdk-fbsvc-5c0c8dcf0f.json");
+const serviceAccount = require("./drive-95bb0-firebase-adminsdk-fbsvc-0cad435f73.json");
 
 initializeApp({
     credential: cert(serviceAccount)
